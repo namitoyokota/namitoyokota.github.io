7136e6ce2c362fe1bfbd41fca071d74f994f17f8
Namito Yokota <namitoyokota@icloud.com>
2025-02-24 18:33:59 -0600

    Future scripts to be written in Python.

---
+++ statistics/completed_tickets.py
@@ -0,0 +1,1 @@
+# TODO: This script retrieves and outputs statistics on Jira tickets that I've completed.
---
+++ statistics/reviewed_prs.py
@@ -0,0 +1,1 @@
+# TODO: This script retrieves and outputs statistics on Azure DevOps pul requests that I've been asked to review.
